7db46980fc5185b2a20505af45f6ab19c264d962
yzakius <isaac@yzakius.me>
2019-01-28 17:54:46 -0300

    Criada app para vídeo incorporado do vimeo
    close #135

---
+++ pypro/aperitivos/tests.py
@@ -0,0 +1,17 @@
+import pytest
+from django.urls import reverse
+
+from pypro.django_assertions import assert_contains
+
+
+@pytest.fixture
+def resp(client):
+    return client.get(reverse('aperitivos:video', args=('motivacao',)))
+
+
+def test_status_code(resp):
+    assert resp.status_code == 200
+
+
+def test_title_video(resp):
+    assert_contains(resp, '<h1>Teste de Vídeo</h1>')
